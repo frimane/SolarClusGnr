e53f88ca5dfd13c4fc15bf42b994118d890fe3be
Âzeddine FRIMANE <azeddine.frimane@yahoo.com>
2019-02-16 02:38:55 +0000

    Update ClusDirGauss.R

--- R/ClusDirGauss.R
+++ R/ClusDirGauss.R
@@ -160,7 +160,7 @@ dpr <- solve(dco)#chol2inv(chol(dco))
 # initial cluster structure
 # -------------------------
 zi <- rep(1, nrow(meas))
-n_el <- 1
+n_el <- nrow(meas)
 B <- D
 h <- list(as.vector(mvtnorm::rmvnorm(1, dav, dco)),
           B*matrix(rWishart(1, D, dco/D), D, D),
